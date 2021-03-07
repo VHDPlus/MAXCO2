c27ae496aa083ddc2bc61953c923ac07fdc51a17
Leon Beier <lbeier@vhdplus.com>
2021-03-07 12:17:29 +0100

    init

---
+++ Software/NIOSDuino/NIOSDuino.cpp
@@ -0,0 +1,25 @@
+
+#include <Arduino.h>
+#include "scd30.h"
+#include <string.h>
+
+SCD30 scd30;
+
+void setup() {
+    Serial0.begin(9600);
+    
+    scd30.i2c_init();
+    scd30.begin();
+}
+
+void loop() {
+    scd30.read();
+    char message[32];
+    sprintf(message, "CO2: %.2fppm", scd30.co2_value());
+    Serial0.println(message);
+    sprintf(message, "Temperature: %.2fC", scd30.temp_value());
+    Serial0.println(message);
+    sprintf(message, "Humidity: %.2f%%", scd30.hum_value());
+    Serial0.println(message);
+    delay(1000);
+}
